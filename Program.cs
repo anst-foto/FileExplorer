08dcab210bc718bb448ec0f3ad22022bcc0ca8b5
Starinin Andrey <starininandrey@gmail.com>
2023-12-25 20:57:07 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var discs = DriveInfo.GetDrives();
+
+foreach (var disc in discs)
+{
+    Console.WriteLine($"{disc.Name}\t|\t{disc.DriveType}\t|\t{disc.DriveFormat}\t|\t{disc.VolumeLabel}");
+
+    try
+    {
+        foreach (var directory in disc.RootDirectory.GetDirectories())
+        {
+            Console.WriteLine($"{directory.Name}\t=\t{directory.UnixFileMode}");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+    try
+    {
+        foreach (var file in disc.RootDirectory.GetFiles())
+        {
+            Console.WriteLine($"{file.Name}\t+\t{file.UnixFileMode}\t+\t{file.Attributes}");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
